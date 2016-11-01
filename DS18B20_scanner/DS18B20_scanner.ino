3eb1ca10999bcf8b289c4dac249ca74cd71cc2b7
Libor Gabaj <libor.gabaj@gmail.com>
2016-11-01 23:47:22 +0100

    Optimized and reformatted code in DS18B20_scanner.

--- DS18B20_scanner/DS18B20_scanner.ino
+++ DS18B20_scanner/DS18B20_scanner.ino
@@ -1,6 +1,6 @@
 /*
   NAME:
-  DS18B20_scanner.ino - Detect DS18B20 sensors on 1-wire bus
+  Scanning 1-wire bus and detecting all DS18x20 sensors on it
 
   DESCRIPTION:
   - The sketch detects all devices on the one wire bus.
@@ -18,23 +18,30 @@
   
   CREDENTIALS:
   Author: Libor Gabaj
-  Version: 1.1.0
-  Created: 25.03.2015
-  Updated: 02.04.2015
  */
-#include <OneWire.h>
-#include <DS18B20.h>
+#include "OneWire.h"
+#include "DS18B20.h"
 
-const byte pinDS18B20 = 4;
+#define SKETCH "DS18B20_scanner 1.2.0"
+#define ONEWIRE_VERSION "OneWire 2.3.2"
 
-DS18B20 ds(pinDS18B20);
+const unsigned char ADDRESS_LEN = 50;       // Expected maximal length of the device address string
+
+// Hardware configuration
+const byte PIN_DS18B20 = 4;
+
+// Measurement
+DS18B20 ds(PIN_DS18B20);
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("DS18B20 scanner");
-    
+  Serial.println(F(SKETCH));
+  Serial.println(F("Libraries:"));
+  Serial.println(F(DS18B20_VERSION));
+  Serial.println(F(ONEWIRE_VERSION));
   // Print header
+  Serial.println(F("---"));
   Serial.println(F("#\tMODEL\tRES\tPOWER\t\tID\tADDRESS"));
 
   // Devices scanning
@@ -49,13 +56,13 @@ void setup()
     // Print family name
     switch(ds.getFamilyCode())
     {
-      case MODEL_DS18S20:
+      case DS18B20_MODEL_DS18S20:
         Serial.print(F("DS18S20"));
         break;
-      case MODEL_DS1820:
+      case DS18B20_MODEL_DS1820:
         Serial.print(F("DS1820"));
         break;
-      case MODEL_DS18B20:
+      case DS18B20_MODEL_DS18B20:
         Serial.print(F("DS18B20"));
         break;
       default:
@@ -83,23 +90,19 @@ void setup()
     Serial.print(ds.getAddressId(), HEX);
     
     // Print address
-    uint8_t address[8];    
-    ds.getAddress(address);    
+    char address[ADDRESS_LEN];    
+    ds.printAddress(address);    
     Serial.print(F("\t"));    
-    for (byte i = 0; i < 8; i++)
-    {
-      Serial.print(F("0x"));
-      if (address[i] < 0x10) Serial.print("0");
-      Serial.print(address[i], HEX);
-      if (i < 7) Serial.print(F(", "));
-    }
-    Serial.println();
+    Serial.println(address);
   }
   // Scan results
   if (count == 0) Serial.println("N/A");
   Serial.print(F("*** Found "));
   Serial.print(count);
-  Serial.println(F(" device(s) ***\n"));
+  Serial.print(F(" device"));
+  if (count > 1) Serial.print(F("s"));
+  Serial.println(F(" ***"));
 }
  
  void loop(){}
+
